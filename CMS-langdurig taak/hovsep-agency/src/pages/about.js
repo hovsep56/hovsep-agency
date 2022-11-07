77f1606443d319329ca9c2a63687c7a0637a3710
hovsep56 <powerlive96@gmail.com>
2022-11-07 16:32:35 +0100

    test

---
+++ CMS-langdurig taak/hovsep-agency/src/pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from'./about.css';
+
+const IndexPage =()=>{
+    return(
+        <main>
+            <title>Home page</title>
+            <h1>Welcome to Smbatian Agency</h1>
+            <p>This website is to display my love for RPG games by making my own personal top 10 RPG games list togheter with some details on the game itself.</p>
+        </main>
+    )
+}
+
+
+export default IndexPage
